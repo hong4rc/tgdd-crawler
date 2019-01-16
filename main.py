843f94805a6625b847971b365b2c0581781e211d
Ｈｏｎｇａｒｃ <aqua.hong.97@gmail.com>
2019-01-17 00:08:33 +0700

    :loud_sound: check all category and log

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ from api import getAllRating, getRating, getProductList
 import json
 from utils import save
 
-category = 42
+def crawlCategory(category):
+  products = getProductList(category)
+  if len(products) == 0:
+    print(category, 'is not exist!!!')
+    return
+  save(products, 'data', 'category_' + str(category) + '.json')
+  for product in products:
+    id = product['id']
+    print(id, 'crawling...')
+    pData = getAllRating(id)
+    save(pData, 'data/category_' + str(category), 'product_' + str(id) + '.json')
 
-products = getProductList(category)
-save(products, 'data', 'category_' + str(category) + '.json')
-for product in products:
-  id = product['id']
-  pData = getAllRating(id)
-  save(pData, 'data/category_' + str(category), 'product_' + str(id) + '.json')
+MAX_CATEGORY = 200
+i = 0
+while i < MAX_CATEGORY:
+  crawlCategory(i)
+  i += 1
